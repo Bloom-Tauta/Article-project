c787c8b432821ed28321c621d2d803528cc82cb5
Bloom <bloomtauta@gmail.com>
2023-03-28 13:49:32 +0300

    commit changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only[:create, :update]
+  resources :users, only:[:create, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
